ff2caac70bd0feecbf362a5882cc7f9be37d2319
Johnbull Biobele Glad <95866950+hendrixthecoder@users.noreply.github.com>
2024-07-22 23:37:37 +0100

    feat: moved to / after log out

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const res = NextResponse.redirect(new URL("/login", request.url));
+  const res = NextResponse.redirect(new URL("/", request.url));
   res.cookies.delete("session_token"); // Clear the session token cookie
   return res;
 }
