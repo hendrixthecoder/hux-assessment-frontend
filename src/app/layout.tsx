9d3c33130055f2673e75c7784937edcf9fe53a8e
Johnbull Biobele Glad <95866950+hendrixthecoder@users.noreply.github.com>
2024-07-23 01:35:55 +0100

    feat: changed to Hoefler font, landing min page styles implemented

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 
-const inter = Inter({ subsets: ["latin"] });
+const myFont = localFont({ src: "../../public/fonts/Hoeflers/Hoeflers.otf" });
 
 export const metadata: Metadata = {
   title: "Phonebook",
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={myFont.className}>
         {children}
         <Toaster />
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+import Lottie from "lottie-react";
+import PhoneAnimation from "../../public/assets/landing-phone.json";
+
 export default function Home() {
-  return <main className="bg-black text-white h-svh">Teardown</main>;
+  return (
+    <main className="h-svh flex w-full bg-[#FADAEA]">
+      <div className="w-full sm:w-1/2 flex h-full items-center ">HI there</div>
+      <div className="hidden sm:flex w-1/2 h-full items-center justify-center">
+        <Lottie animationData={PhoneAnimation} />
+      </div>
+    </main>
+  );
 }
